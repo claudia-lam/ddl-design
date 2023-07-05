6c971a16f2a2ee3f031e99918c57d9e14340b63a
Claudia Lam <claudiaslam@gmail.com>
2023-07-05 01:10:31 -0700

    fix semicolons and commas

--- outer_space.sql
+++ outer_space.sql
@@ -1,15 +1,16 @@
 CREATE TABLE moons(
     name TEXT PRIMARY KEY NOT NULL UNIQUE
-)
+);
 
 CREATE TABLE stars(
-    name TEXT PRIMARY KEY NOT NULL UNIQUE
+    name TEXT PRIMARY KEY NOT NULL UNIQUE,
     temp_in_kelvin NUMERIC NOT NULL
-)
+);
 
 CREATE TABLE planets(
     name TEXT PRIMARY KEY NOT NULL UNIQUE,
     orbital_period INT NOT NULL,
     star TEXT REFERENCES stars,
     moons TEXT REFERENCES moons
-)
+);
+

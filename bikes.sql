753f2a4c017324b632df24d22be84efe937d6979
Claudia Lam <claudiaslam@gmail.com>
2023-07-05 19:17:38 -0700

    add schema for bikes and orders

--- bikes.sql
+++ bikes.sql
@@ -3,3 +3,19 @@ CREATE TABLE customers (
     first_name VARCHAR(25) NOT NULL,
     last_name VARCHAR(25) NOT NULL
 );
+
+CREATE TABLE bikes(
+    id VARCHAR(25) PRIMARY KEY,
+    bike_name VARCHAR(25) NOT NULL,
+    cost NUMERIC NOT NULL
+)
+
+CREATE TABLE orders(
+    id SERIAL PRIMARY KEY,
+    bike_model TEXT REFERENCES bikes NOT NULL,
+    order_date TIMESTAMP WITH TIME ZONE NOT NULL,
+    note VARCHAR(25) NOT NULL,
+    total_cost NUMERIC REFERENCES bikes NOT NULL,
+    customer TEXT REFERENCES customers NOT NULL
+)
+
